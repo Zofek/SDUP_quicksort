6a407e0607de38b94a00db7a1c9655b24a7c6b6f
Zofek <87820675+Zofek@users.noreply.github.com>
2023-05-02 11:33:35 +0200

    partition
    simplified version of partition

--- partition.v
+++ partition.v
@@ -8,31 +8,27 @@ module partition#(
     input wire [ARR_WIDTH*4-1:0] array_in, 
     input wire 					clock, 
     input wire 					reset,
-    input wire 					hi_ind,
-    input wire 					lo_ind,
-    input wire                  curr_pivot,
+    input wire [3:0]		    hi_ind,
+    input wire [3:0]			lo_ind,
 	input wire [0:0]            start,
-	input wire                  index_in,
-	input wire                  temp_ctr_in, 
 	
     output reg [ARR_WIDTH*4-1:0] array_out,
-    output reg 				    pivot,
     output reg [0:0]            ready,
-    output reg                  index_out,
-    output reg                  temp_ctr_out
+    output reg [3:0]            pivot_ind
     );
     
 //--------------------------------------------	
-	reg [3:0] array_out_nxt [ARR_WIDTH*4-1:0];
-	reg pivot_nxt, ind_out_nxt, temp_ctr_out_nxt;
+	reg [3:0] array_out_2d  [ARR_WIDTH-1:0];
+    reg [3:0] array_out_2d_nxt [ARR_WIDTH-1:0];
+	reg [3:0] pivot_ind_nxt, counter, counter_nxt;
 	reg [0:0] ready_nxt;
-	reg [1:0] state, state_nxt;
+	reg [0:0] state, state_nxt;
 	
 // STATE MACHINE -----------------------------
      
     //states
-    localparam IDLE               = 2'b01,
-               PARTITION_PENDING  = 2'b10;
+    localparam IDLE               = 1'b0,
+               PARTITION_PENDING  = 1'b1;
 
 //seq logic    
 //--------------------------------------------
@@ -40,30 +36,31 @@ module partition#(
 	begin
 	if (reset)
 		begin
-		array_out     <= {ARR_WIDTH*4-1{1'b0}};
-		array_out_nxt[0] <= 3'b0;
-		array_out_nxt[1] <= 3'b0;
-		array_out_nxt[2] <= 3'b0;
-		array_out_nxt[3] <= 3'b0;
+		array_out        <= {ARR_WIDTH*4-1{1'b0}};
+		array_out_2d[0]  <= 3'b0;
+		array_out_2d[1]  <= 3'b0;
+		array_out_2d[2]  <= 3'b0;
+		array_out_2d[3]  <= 3'b0;
 		array_out        <= 0;
-		pivot 		     <= 0;
-		index_out        <= 0;
-        temp_ctr_out     <= 0;
+		pivot_ind 		 <= 0;
         ready            <= 0;
+        counter          <= 0;
         state            <= IDLE;
 		end 
 	else 
 		begin
-		array_out[3:0]   <= array_out_nxt[0];
-        array_out[7:4]   <= array_out_nxt[1];
-        array_out[11:8]  <= array_out_nxt[2];
-        array_out[15:12] <= array_out_nxt[3];
-		pivot 		     <= pivot_nxt;
+		array_out_2d[0]  <= array_out_2d_nxt[0];
+        array_out_2d[1]  <= array_out_2d_nxt[1];
+        array_out_2d[2]  <= array_out_2d_nxt[2];
+        array_out_2d[3]  <= array_out_2d_nxt[3];
+        array_out[3:0]   <= array_out_2d[3];
+        array_out[7:4]   <= array_out_2d[2];
+        array_out[11:8]  <= array_out_2d[1];
+        array_out[15:12] <= array_out_2d[0];
+		pivot_ind 		 <= pivot_ind_nxt;
 		ready            <= ready_nxt;
-		index_out        <= ind_out_nxt;
-		temp_ctr_out     <= temp_ctr_out_nxt;
+		counter          <= counter_nxt;
 		state            <= state_nxt;
-		
 		end 
 	end 
 	
@@ -72,19 +69,22 @@ module partition#(
 	always @*
 	   
         begin
-        array_out_nxt[0]  = array_in[3:0];
-        array_out_nxt[1]  = array_in[7:4];
-        array_out_nxt[2]  = array_in[11:8];
-        array_out_nxt[3]  = array_in[15:12];
         
         case (state)
         
         IDLE:
             begin
-            ind_out_nxt      = index_in;
-            pivot_nxt        = curr_pivot;
-            temp_ctr_out_nxt = temp_ctr_in; 
+            array_out_2d[3]      = array_in[3:0];
+            array_out_2d[2]      = array_in[7:4];
+            array_out_2d[1]      = array_in[11:8];
+            array_out_2d[0]      = array_in[15:12];
+            array_out_2d_nxt[3]  = array_in[3:0];
+            array_out_2d_nxt[2]  = array_in[7:4];
+            array_out_2d_nxt[1]  = array_in[11:8];
+            array_out_2d_nxt[0]  = array_in[15:12];
+            pivot_ind_nxt    = lo_ind - 1;
             ready_nxt        = 1'b0;
+            counter_nxt      = 0;
             
             if (start == 1) state_nxt = PARTITION_PENDING;
             else            state_nxt = IDLE;      
@@ -95,28 +95,27 @@ module partition#(
             begin
             ready_nxt = 1'b0;
             state_nxt = PARTITION_PENDING;
-            
-            if (temp_ctr_out < hi_ind)
+            if (counter < hi_ind)
                 begin
-                temp_ctr_out_nxt = temp_ctr_in + 1;
-                if (array_in[temp_ctr_in] <= pivot_nxt)
+                counter_nxt = counter + 1;
+                if (array_out_2d[counter] <= array_out_2d[hi_ind])
                     begin
-                    ind_out_nxt = index_in + 1;
-                    array_out_nxt[ind_out_nxt] = array_out_nxt[temp_ctr_out_nxt];
-                    array_out_nxt[temp_ctr_out_nxt] = array_out_nxt[ind_out_nxt];
+                    pivot_ind_nxt = pivot_ind + 1;
+                    array_out_2d_nxt[pivot_ind_nxt] = array_out_2d[counter];
+                    array_out_2d_nxt[counter]       = array_out_2d[pivot_ind_nxt];
                     end 
                 end 
             else 
                 begin
+                pivot_ind_nxt = pivot_ind + 1;
+                array_out_2d_nxt[pivot_ind_nxt] = array_out_2d[hi_ind];
+                array_out_2d_nxt[hi_ind] = array_out_2d[pivot_ind_nxt];    
                 state_nxt = IDLE;
                 ready_nxt = 1'b1;
                 end
-                
-            ind_out_nxt = ind_out_nxt + 1;
-            array_out_nxt[ind_out_nxt] = array_out_nxt[hi_ind];
-            array_out_nxt[hi_ind] = array_out_nxt[ind_out_nxt];                
+                     
             end 
- 
+        
         endcase
         end //always @*
 
